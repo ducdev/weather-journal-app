26ed4829561cf1b8ce67da61c05ca43c7a98f261
Duc Le <leanhducvt@live.com>
2020-02-23 17:11:21 +0700

    feat: Add a GET route that returns the projectData object in server code, add a POST route that adds incoming data to projectData

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 // Setup empty JS object to act as endpoint for all routes
-projectData = {
-  foo: 'bar'
-};
+projectData = [];
 
 // port
 const port = 3000
@@ -24,9 +22,24 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 app.use(cors());
 
+// routes
+
+app.get('/get', (req, res) => res.send(projectData));
+
+const add = (req, res) => {
+  const newObject = {
+    zip: req.body.zip,
+    feeling: req.body.feeling,
+  };
+
+  projectData.push(newObject);
+  res.send(projectData);
+}
+
+app.post('/post', add);
+
+
 // Initialize the main project folder
 app.use(express.static('website'));
 
 app.listen(port, listening);
-
-// Setup Server
