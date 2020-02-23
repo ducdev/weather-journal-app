a08a6d0411e925e2c5870a3171774bceffda533f
Duc Le <leanhducvt@live.com>
2020-02-23 16:47:19 +0700

    feat: setting up your project environment

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-// Require Express to run server and routes
+// Setup empty JS object to act as endpoint for all routes
+projectData = {
+  foo: 'bar'
+};
+
+// port
+const port = 3000
+
+// listening
+const listening = () => console.log(`Started on localhost:${port}`)
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +22,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+app.listen(port, listening);
 
 // Setup Server
